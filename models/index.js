fa2466dda1b98af68ca4e71e77c9147ef13ddf13
janiceh94 <haesung94@gmail.com>
2022-01-30 07:38:53 -0800

    Created config folder

--- models/index.js
+++ models/index.js
@@ -1,15 +1,4 @@
-const mongoose = require('mongoose');
-const db = mongoose.connection;
-const dbURL = process.env.MONGO_URL;
-
-mongoose
-    .connect(dbURL)
-    .then(() =>
-        console.log(`MongoDB successfully connected at ${db.host}:${db.port}!`)
-    )
-    .catch((err) => console.log(`MongoDB connection FAILED :( Error: ${err})`));
-
-    module.exports = {
+module.exports = {
         Homecook: require("./homecook"),
         Comment: require("./comment"),
         Recipe: require("./recipe")
