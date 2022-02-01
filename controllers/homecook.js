345d7d600005b1e561392dc4ceb5186dfd29902e
janiceh94 <haesung94@gmail.com>
2022-01-31 16:03:35 -0800

    Fixed error in controllers/homecook.js

--- controllers/homecook.js
+++ controllers/homecook.js
@@ -10,4 +10,6 @@ function index(req, res, next) {
     });
 }
 
-module.export = index;
+module.exports = {
+  index
+};
--- routes/homecook.js
+++ routes/homecook.js
@@ -1,13 +1,15 @@
 const router = require("express").Router();
-const homecookCtrl = require('../controllers/index');
-
-/* router.get("/homecook", isLoggedIn, homecookCtrl.index); */
-/* router.post("/recipe", isLoggedIn, homecookCtrl.newRecipe);
-router.delete("/recipe/:id", isLoggedIn, homecookCtrl.deleteRecipe); */
+const homecookCtrl = require('../controllers/homecook');
 
 function isLoggedIn(req, res, next) {
     if (req.isAuthenticated()) return next();
     res.redirect("/auth/google");
   }
+
+router.get("/homecook", isLoggedIn, homecookCtrl.index);
+
+router.post("/recipe", isLoggedIn, homecookCtrl.newRecipe);
+
+router.delete("/recipe/:id", isLoggedIn, homecookCtrl.deleteRecipe);
 
 module.exports = router;
