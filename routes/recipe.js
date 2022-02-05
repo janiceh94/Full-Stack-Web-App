5238e46aff6134d8f50e0c275d19aa38e4b5146f
janiceh94 <haesung94@gmail.com>
2022-02-05 08:00:26 -0800

    Delete recipe button

--- routes/recipe.js
+++ routes/recipe.js
@@ -1,13 +1,18 @@
 const router = require('express').Router();
 const ctrl = require('../controllers');
 
-router.get('/recipes', ctrl.recipes.idx);
-router.get('/recipes/new', ctrl.recipes.newRecipe);
-router.get('/recipes/:id', ctrl.recipes.show);
-router.post('/recipes', ctrl.recipes.createRecipe);
-router.get('/recipes/:id/edit', ctrl.recipes.edit);
-router.put('/recipes/:id', ctrl.recipes.update);
-router.delete('/recipes/:id', ctrl.recipes.deleteRecipe);
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()) return next();
+    res.redirect('/auth/google');
+}
+
+router.get('/recipes', isLoggedIn, ctrl.recipes.idx);
+router.get('/recipes/new', isLoggedIn, ctrl.recipes.newRecipe);
+router.get('/recipes/:id', isLoggedIn, ctrl.recipes.show);
+router.post('/recipes', isLoggedIn, ctrl.recipes.createRecipe);
+router.get('/recipes/:id/edit', isLoggedIn, ctrl.recipes.edit);
+router.put('/recipes/:id', isLoggedIn, ctrl.recipes.update);
+router.delete('/recipes/:id', isLoggedIn, ctrl.recipes.deleteRecipe);
 
 module.exports = router;
 
